d3d52ba35f7b7bd0299ce13d054d6e5d3d9803bd
GustavoHen12 <gustavobarbosa@alunos.utfpr.edu.br>
2020-03-05 11:00:53 -0300

    reorganiza direc. e nova pag

--- main/js/funcoes.js
+++ main/js/funcoes.js
@@ -1,15 +1,33 @@
 function getPath () {
     const app = require('electron').remote.app;
     let caminho = app.getAppPath();
-    let novocaminho = caminho.concat("/libs/data");
-    return novocaminho;
+    return caminho;
 }
 
-const dataC = require(getPath());
+let path = getPath();
+const dataC = require(path.concat("/libs/data"));
 let DataControler = dataC.DataControler;
+
+const struct = require(path.concat("/libs/dataType"));
+let alunoData = struct.makeDataStruct("id nome turno turma prof defciencia escola");
+
 let teste = new DataControler("lista1");
 
 function sendNew()
 {
     console.log("oi");
+    let elemento = document.getElementById("nome");
+    let nome = elemento.value;
+    elemento = document.getElementById("deficiencia");
+    let deficiencia = elemento.value;
+    elemento = document.getElementById("turno");
+    let turno = elemento.value;
+    elemento = document.getElementById("turma");
+    let turma = elemento.value;
+    elemento = document.getElementById("prof");
+    let prof = elemento.value;
+    let id = "1";
+    let newAluno = new alunoData(id, nome, turno, turma, prof, deficiencia);
+    console.log(newAluno);
+    teste.sendData(newAluno);
 }
