d5a6e58d3ad426040076f7325e66ee73a81b6b4d
GustavoHen12 <gustavobarbosa@alunos.utfpr.edu.br>
2020-03-08 19:59:36 -0300

    adiciona relatorios

--- libs/objects.js
+++ libs/objects.js
@@ -17,11 +17,20 @@ class construtorAlunos
                                 '<p id = "Adef">'+dado.defciencia+'</p>'+
                             '</div>'+
                             '<div class = "acoesAluno">'+
-                                '<button id = "plusButton" class = "botaoAluno" onclick="plusButton('+ dado.id + ')"><img src="img/plus.svg"></button>'+
+                                '<button id = "plusButton" class = "botaoAluno" onclick="plusButton('+ dado.id +')"><img src="img/plus.svg"></button>'+
                                 '<button id = "relatoryButton" onclick="relatoryButton('+ dado.id + ')"><img src="img/relatory.svg"></button>'+
                                 '<button id = "editButton" onclick="editButton('+ dado.id + ')"><img src="img/edit.svg"></button>'+
                                 '<button id = "trianguloButton" onclick="trianguloButton('+ dado.id + ')"><img src="img/triangulo.svg"></button>'+
                             '</div>'+
+                            '<div id = "plus'+ dado.id + '"class = "plusArea">'+
+                                '<div class = "opButtons">'+
+                                    '<button onclick="sendRelatory('+dado.id+')">Salvar</button>'+
+                                    '<button onclick="plusButton('+dado.id+')">Cancelar</button>'+
+                                '</div>'+
+                                '<div id = "addRelatory" class = "inputContainer">'+
+                                    '<textarea rows="6" id="add'+dado.id+'" style="width: 99%" value=""></textarea>'+
+                                '</div>'+
+                            '</div>' +
                         '</div>';
         el.innerHTML =  domString;
         return el.firstChild;
--- main/js/iniciaObjetos.js
+++ main/js/iniciaObjetos.js
@@ -4,8 +4,10 @@ let path = app.getAppPath();
 const struct = require(path.concat("/libs/dataType"));
 //constroi objeto para dados basicos dos alunos
 let alunoData = struct.makeDataStruct("id nome turno turma prof defciencia escola");
+let relatorioData = struct.makeDataStruct("id tempo texto");
 
 //constroi objetos que manipulam os arquivos
 const dataC = require(path.concat("/libs/data"));
 let DataControler = dataC.DataControler;
 let alunos = new DataControler("listaAlunos");
+let relatorio = new DataControler("relatoriosAlunos");
