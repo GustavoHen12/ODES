1442d115ff02a57133eead3a9c05708f2dc262a5
GustavoHen12 <gustavobarbosa@alunos.utfpr.edu.br>
2020-03-08 18:00:47 -0300

    para iniciar os objetos para dados

---
+++ main/js/iniciaObjetos.js
@@ -0,0 +1,11 @@
+const app = require('electron').remote.app;
+let path = app.getAppPath();
+
+const struct = require(path.concat("/libs/dataType"));
+//constroi objeto para dados basicos dos alunos
+let alunoData = struct.makeDataStruct("id nome turno turma prof defciencia escola");
+
+//constroi objetos que manipulam os arquivos
+const dataC = require(path.concat("/libs/data"));
+let DataControler = dataC.DataControler;
+let alunos = new DataControler("listaAlunos");
